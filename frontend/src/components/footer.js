8ae67919c8f8f0b92c2305e6ef5fda50c6647cdc
kamitssm123 <57366324+kamitssm123@users.noreply.github.com>
2022-12-21 15:47:31 +0530

    Update footer.js

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
     render() {
         return (
             <div className="footer">
-                <div className="text-white text-center" style={{fontSize: 'smaller'}}>Made with <span role="img" aria-label="emoji">&#128151;</span> by: Akshay Jain, IIT Kharagpur</div>
+                <div className="text-white text-center" style={{fontSize: 'smaller'}}>Made with <span role="img" aria-label="emoji">&#128151;</span> by: Amit Kumar, IIT Kharagpur</div>
             </div>
         )
     }
